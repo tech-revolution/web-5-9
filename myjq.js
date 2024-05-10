054cb054113acc951fff305aee497065f8430206
Teacher <odootest90@gmail.com>
2024-05-10 19:30:25 +0500

    Added class 37 jQuery and AJAX

---
+++ myjq.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    // $("#clickme").click(function(){
+    //     $("#slidedown").slideToggle(5000, function(){
+    //         alert("Task is completed")
+    //     });
+        
+    // })
+    // $("#btn").click(function(){
+    //     $("#slidedown").stop();
+    // })
+    // $("#btn1").click(function(){
+    //     $("#div1").load("demo.txt", function(responseTxt, statusTxt, xhr){
+    //         if(statusTxt == "success"){
+    //             alert("Content loaded successfuly");
+    //         }
+    //         if(statusTxt == "error")
+    //             alert(`Error ${xhr.status} and ${xhr.statusTxt}`);
+    //     })
+    // })
+
+    $("#btn2").click(function(){
+        // $("ol").append("<li>Demo</li>")
+        $("li").remove()
+    })
+
+
+
+})
